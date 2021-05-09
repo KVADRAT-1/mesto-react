9f295c89dfd3ba184e48343ab0264f54489088e8
KVADRAT-1 <61181626+KVADRAT-1@users.noreply.github.com>
2021-05-09 15:56:28 +0300

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+import PopupWithForm from './PopupWithForm';
+import ImagePopup from './ImagePopup';
+
+function App() {
+    const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
+    const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
+    const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
+    const [selectedCard, setSelectedCard] = React.useState({state:false});
+
+    function handleEditAvatarClick() {
+        setIsEditAvatarPopupOpen(!isEditAvatarPopupOpen);
+    }
+
+    function handleEditProfileClick() {
+        setIsEditProfilePopupOpen(!isEditProfilePopupOpen);
+    }
+
+    function handleAddPlaceClick() {
+        setIsAddPlacePopupOpen(!isAddPlacePopupOpen);
+    }
+
+    function handleCardClick(card) {
+        setSelectedCard({state: true, name: card.name, link: card.link})
+    }
+
+    function closeAllPopups() {
+        setIsEditAvatarPopupOpen(false);
+        setIsEditProfilePopupOpen(false);
+        setIsAddPlacePopupOpen(false);
+        setSelectedCard({state: false});
+    }
+
+  return (
+    <div className='App'>
+      <div className='page'>
+        <Header />
+        <Main onEditAvatar={handleEditAvatarClick} onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick} onCardClick={handleCardClick} />
+        <Footer />
+        <PopupWithForm title={'Обновить аватар'} name={'change-avatar'} buttonText={'Сохранить'} isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups} children={
+            <div className="popup__section">
+                <input id="popup__input_link_avatar" className="popup__input popup__input_link_picture" name="popup__input_link_picture" type="url" placeholder="Ссылка на новый аватар" required/>
+                <span id="popup__input_link_avatar-error" className="popup__input-error"></span>
+            </div>
+        }/>
+        <PopupWithForm title={'Редактировать профиль'} name={'change-profile'} buttonText={'Сохранить'} isOpen={isEditProfilePopupOpen} onClose={closeAllPopups} children={
+            <>
+                <div className="popup__section">
+                    <input id="popup__input_text_name" className="popup__input popup__input_text_name" name="nameProfile" type="text" required minLength="2" maxLength="40" placeholder="Имя"/>
+                    <span id="popup__input_text_name-error" className="popup__input-error"></span>
+                </div>
+                <div className="popup__section">
+                    <input id="popup__input_text_description" className="popup__input popup__input_text_description" name="nameAbout" type="text" required minLength="2" maxLength="200" placeholder="Описание"/>
+                    <span id="popup__input_text_description-error" className="popup__input-error"></span>
+                </div>
+            </>
+        }/>
+        <PopupWithForm title={'Новое место'} name={'addition-picture'} buttonText={'Сохранить'} isOpen={isAddPlacePopupOpen} onClose={closeAllPopups} children={
+            <>
+                <div className="popup__section">
+                    <input id="popup__input_name_picture" className="popup__input popup__input_name_picture" name="namePicture" type="text" placeholder="Название" required minLength="2" maxLength="30"/>
+                    <span id="popup__input_name_picture-error" className="popup__input-error"></span>
+                </div>
+                <div className="popup__section">
+                    <input id="popup__input_link_picture" className="popup__input popup__input_link_picture" name="nameLink" type="url" placeholder="Ссылка на картинку" required/>
+                    <span id="popup__input_link_picture-error" className="popup__input-error"></span>
+                </div>
+            </>
+        }/>
+        <PopupWithForm title={'Вы уверены ?'} name={'delete-picture'} buttonText={'Да'} children={<></>}/>
+        <ImagePopup card={selectedCard} onClose={closeAllPopups}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/utils/constants.js
@@ -0,0 +1,34 @@
+export const popupProfile = document.querySelector("#popup__profile");
+export const popupAddition = document.querySelector("#popup__addition");
+export const popupPicture = document.querySelector("#popup__picture");
+export const popupDeletePicture = document.querySelector(
+  "#popup__deletePicture"
+);
+export const popupProfileAvatar = document.querySelector(
+  "#popup__profileAvatar"
+);
+export const editButton = document.querySelector(".profile__edit-buttom");
+export const popupFormProfile = document.querySelector("#popup__form-profile");
+export const popupFormPicture = document.querySelector("#popup__form-picture");
+export const profileName = document.querySelector(".profile__name");
+export const profileDescription = document.querySelector(
+  ".profile__description"
+);
+export const popupInputTextName = document.querySelector(
+  ".popup__input_text_name"
+);
+export const popupInputTextDescription = document.querySelector(
+  ".popup__input_text_description"
+);
+export const profileAddButton = document.querySelector(".profile__add-button");
+export const elementsList = document.querySelector(".elements__list");
+export const namePicture = document.querySelector("#popup__input_name_picture");
+export const linkPicture = document.querySelector("#popup__input_link_picture");
+export const popupSubmitButtonPicture = document.querySelector(
+  "#popup__submit-button-picture"
+);
+export const classElementsTemplate = "#elements-template";
+export const avatar = document.querySelector(".profile__avatar");
+export const avatarContainer = document.querySelector(
+  ".profile__avatar-container"
+);
